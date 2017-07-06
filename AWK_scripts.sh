6ddae9af94076f22f384278a14ccb69bbbb126e3
Paulo Gervasio <paulo_gervasio@Hotmail.com>
2017-07-06 10:25:54 -0300

    Add new comment
    Add new comment

--- AWK_scripts.sh
+++ AWK_scripts.sh
@@ -8,6 +8,7 @@ BASH/AWK - Rename file names from a directory
 #VERSÃO FINAL:
 final=`ls -1 | awk '{ORS=" "; print "mv "$1; gsub("jar.src.zip.src.zip", "jar.src.zip", $1);ORS=";"; print $1};'` | eval $final
 
+# compila o script todo em uma única linha e depois envia para o comando eval que faz o parse dos comandos.
 
 
 Testes:
