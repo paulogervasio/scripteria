7e339a8c6dfe4409b6c3361c5654b63932d8a3d6
Paulo Gervasio <paulo_gervasio@Hotmail.com>
2017-07-06 10:23:36 -0300

    Script bash/AWK - Added
    Script bash/AWK que faz replace em múltiplos arquivos no mesmo diretório

---
+++ AWK_scripts.sh
@@ -0,0 +1,17 @@
+﻿#########################################################################################################################
+BASH/AWK - Rename file names from a directory
+#########################################################################################################################
+
+# A ideia inicial foi renomear os arquivos com extensão duplicada "jar.src.zip.src.zip" para "jar.src.zip".
+
+
+#VERSÃO FINAL:
+final=`ls -1 | awk '{ORS=" "; print "mv "$1; gsub("jar.src.zip.src.zip", "jar.src.zip", $1);ORS=";"; print $1};'` | eval $final
+
+
+
+Testes:
+# Exibe o replace feito, sem alterar o arquivo - (APENAS TESTES)
+ls -1 | awk '{ORS=" "; print "mv "$1; 	("jar.src.zip.src.zip", "jar.src.zip", $1);ORS=";"; print $1};'
+
+
